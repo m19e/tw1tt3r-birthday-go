557d381971ce2cf4904f9b477f36005f3c5b5fc6
m19e <nexus.me7290@gmail.com>
2019-12-25 02:32:52 +0900

    ID指定で誕生日を取得

--- twitter.go
+++ twitter.go
@@ -4,12 +4,22 @@ import (
 	"github.com/joho/godotenv"
 	//_ "github.com/joho/godotenv/autoload"
 	"github.com/ChimeraCoder/anaconda"
-	
-	"log"
-	. "os"
+	"github.com/PuerkitoBio/goquery"
+
+	"encoding/json"
 	f "fmt"
+	"log"
 	"net/url"
+	. "os"
+	"strings"
 )
+
+type BD struct {
+	Name string
+	Date string
+}
+
+type BDList []BD
 
 func loadEnv() {
 	err := godotenv.Load()
@@ -19,7 +29,25 @@ func loadEnv() {
 }
 
 func GetTwitterApi() *anaconda.TwitterApi {
-	return anaconda.NewTwitterApiWithCredentials(Getenv("ACCESS_TOKEN_KEY"), Getenv("ACCESS_TOKEN_SECRET"), Getenv("CONSUMER_KEY"), Getenv("CONSUMER_SECRET"))
+	return anaconda.NewTwitterApiWithCredentials(
+		Getenv("ACCESS_TOKEN_KEY"),
+		Getenv("ACCESS_TOKEN_SECRET"),
+		Getenv("CONSUMER_KEY"),
+		Getenv("CONSUMER_SECRET"),
+	)
+}
+
+func GetBirthday(id string, target string) BDList {
+	var bdlist BDList
+	doc, _ := goquery.NewDocument("https://twitter.com/" + id)
+	doc.Find("span").Each(func(_ int, s *goquery.Selection) {
+		elem := s.Text()
+		if strings.Contains(elem, target) {
+			e := strings.TrimSpace(elem)
+			bdlist = append(bdlist, BD{Name: id, Date: e})
+		}
+	})
+	return bdlist
 }
 
 func main() {
@@ -32,11 +60,15 @@ func main() {
 	v.Set("screen_name", "imassc_official")
 
 	//q := "ゆゆ式 -RT"
-	
-	// result, _ := api.GetSearch("#ゆゆ式ac -RT", v)
+
+	// result, _ := api.GetSearch(q, v)
 	result, _ := api.GetUserTimeline(v)
 	for _, tweet := range result {
 		f.Println("----------")
 		f.Println(tweet.FullText)
 	}
+
+	bd := GetBirthday("TwitterJP", "誕生日")
+	b, _ := json.Marshal(bd)
+	f.Printf("%s\n", b)
 }
