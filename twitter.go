e65487f89a9d18ca6e5e71efee82c0d2d634de11
m19e <nexus.me7290@gmail.com>
2019-12-24 23:10:11 +0900

    dotenvを横着せずちゃんと読み込む

--- twitter.go
+++ twitter.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	//"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
+	//_ "github.com/joho/godotenv/autoload"
 	"github.com/ChimeraCoder/anaconda"
 	
-	_ "log"
+	"log"
 	. "os"
 	f "fmt"
 	"net/url"
 )
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed loading .env file")
+	}
+}
 
 func GetTwitterApi() *anaconda.TwitterApi {
 	return anaconda.NewTwitterApiWithCredentials(Getenv("ACCESS_TOKEN_KEY"), Getenv("ACCESS_TOKEN_SECRET"), Getenv("CONSUMER_KEY"), Getenv("CONSUMER_SECRET"))
 }
 
 func main() {
+	loadEnv()
+
 	api := GetTwitterApi()
 
 	v := url.Values{}
