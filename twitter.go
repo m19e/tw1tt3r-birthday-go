4f539a0b7237a833351f2ac868a5c8a3a4fa2b65
m19e <nexus.me7290@gmail.com>
2019-12-29 05:17:03 +0900

    GetUsersLookupByIdsでユーザー情報を取得
    Chunksをチャンネルではなく配列にした
    速度が出ないようなら戻すかもしれない

--- twitter.go
+++ twitter.go
@@ -12,14 +12,18 @@ import (
 	"net/url"
 	. "os"
 	"strings"
+	"sync"
 )
 
-type BD struct {
+type User struct {
+	SN   string
 	Name string
-	Date string
 }
 
-type BDList []BD
+type BD struct {
+	USER User
+	Date string
+}
 
 func loadEnv() {
 	err := godotenv.Load()
@@ -57,34 +61,51 @@ friendsLoop:
 	return friends
 }
 
-func GetBirthday(sn string, target string) BDList {
-	var bdlist BDList
+// func GetAllUsersLookup(f func(), idsList [][]int64) [][]anaconda.User {
+// 	var users [][]anaconda.User
+// 	var wg sync.WaitGroup
+// 	v := url.Values{}
+// 	v.Set("include_entities", "false")
+
+// 	for _, ids := range idsList {
+// 		wg.Add(1)
+// 		go func(ids []int64) {
+// 			defer wg.Done()
+// 			res, _ := api.GetUsersLookupByIds(ids, v)
+// 			users = append(users, res)
+// 		}(ids)
+// 	}
+
+// 	wg.Wait()
+// 	return users
+// }
+
+func GetBirthday(sn string, target string) []BD {
+	var bdlist []BD
 	doc, _ := goquery.NewDocument("https://twitter.com/" + sn)
 	doc.Find("span").Each(func(_ int, s *goquery.Selection) {
 		elem := s.Text()
 		if strings.Contains(elem, target) {
 			e := strings.TrimSpace(elem)
-			bdlist = append(bdlist, BD{Name: sn, Date: e})
+			bdlist = append(bdlist, BD{USER: User{SN: sn}, Date: e})
 		}
 	})
 	return bdlist
 }
 
-func Chunks(l []int64, n int) chan []int64 {
-	ch := make(chan []int64)
+func Chunks(l []int64, n int) [][]int64 {
+	var result [][]int64
 
-	go func() {
-		for i := 0; i < len(l); i += n {
-			from := i
-			to := i + n
-			if to > len(l) {
-				to = len(l)
-			}
-			ch <- l[from:to]
+	for i := 0; i < len(l); i += n {
+		from := i
+		to := i + n
+		if to > len(l) {
+			to = len(l)
 		}
-		close(ch)
-	}()
-	return ch
+		result = append(result, l[from:to])
+	}
+
+	return result
 }
 
 func main() {
@@ -96,22 +117,13 @@ func main() {
 	v.Set("count", "5000")
 	v.Set("screen_name", "imassc_official")
 
-	//q := "ゆゆ式 -RT"
-
-	// result, _ := api.GetSearch(q, v)
-	//	result, _ := api.GetUserTimeline(v)
-	//	for _, tweet := range result {
-	//		f.Println("----------")
-	//		f.Println(tweet.FullText)
-	//	}
-
 	bd := GetBirthday("TwitterJP", "誕生日")
 	b, _ := json.Marshal(bd)
 	f.Printf("%s\n", b)
 
-	ids := api.GetFriendsIdsAll(v)
+	idsAll := api.GetFriendsIdsAll(v)
 
-	friends := GetFriendsIdList(ids)
+	friends := GetFriendsIdList(idsAll)
 
 	f.Printf("length: %d\n", len(friends))
 	for _, n := range friends {
@@ -119,8 +131,33 @@ func main() {
 	}
 
 	size := 100
-	for l := range Chunks(friends, size) {
-		f.Printf("length: %d\n", len(l))
+	chunked := Chunks(friends, size)
+	for _, l := range chunked {
 		f.Println(l)
 	}
+
+	var users [][]anaconda.User
+	var wg sync.WaitGroup
+	p := url.Values{}
+	p.Set("include_entities", "false")
+
+	for _, ids := range chunked {
+		wg.Add(1)
+		log.Print(ids)
+		go func(ids []int64) {
+			defer wg.Done()
+			res, err := api.GetUsersLookupByIds(ids, p)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, res)
+		}(ids)
+	}
+
+	wg.Wait()
+
+	u, _ := json.Marshal(users)
+	f.Printf("%s\n", u)
+
+	f.Printf("%s\n", users[0][0].Name)
 }
